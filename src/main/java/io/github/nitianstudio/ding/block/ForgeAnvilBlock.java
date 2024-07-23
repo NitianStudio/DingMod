c4f0a260078c1d79542a31592f9b2c1bf1aee645
baka4n <qq474899581@gmail.com>
2024-07-23 22:44:56 +0800

    修缮模型中

--- src/main/java/io/github/nitianstudio/ding/block/ForgeAnvilBlock.java
+++ src/main/java/io/github/nitianstudio/ding/block/ForgeAnvilBlock.java
@@ -1,17 +1,61 @@
 package io.github.nitianstudio.ding.block;
 
+import com.mojang.serialization.MapCodec;
 import io.github.nitianstudio.ding.registry.RegistryBlockTile;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ForgeAnvilBlock extends Block implements EntityBlock {
+public class ForgeAnvilBlock extends FallingBlock implements EntityBlock {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    public static final MapCodec<FallingBlock> CODEC = simpleCodec(ForgeAnvilBlock::new);
     public ForgeAnvilBlock(Properties properties) {
-        super(properties);
+        super(properties.noOcclusion());
+        this.registerDefaultState(this.getStateDefinition().any().setValue(FACING, Direction.SOUTH));
+    }
+
+    @Override
+    protected @NotNull MapCodec<? extends FallingBlock> codec() {
+        return CODEC;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected @NotNull BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    protected BlockState mirror(BlockState state, Mirror mirror) {
+
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection());
+    }
+
+    @Override
+    protected @NotNull VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return super.getShape(state, level, pos, context);
     }
 
     @Nullable
