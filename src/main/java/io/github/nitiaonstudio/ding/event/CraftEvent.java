49dbfbdaf874e03b692482afef9cfd1689eae311
baka4n <qq474899581@gmail.com>
2024-09-17 16:31:31 +0800

    修复可能出现的数据溢出

--- src/main/java/io/github/nitiaonstudio/ding/event/CraftEvent.java
+++ src/main/java/io/github/nitiaonstudio/ding/event/CraftEvent.java
@@ -1,5 +1,6 @@
 package io.github.nitiaonstudio.ding.event;
 
+import io.github.nitiaonstudio.ding.Ding;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
@@ -15,15 +16,31 @@ public class CraftEvent {
         Container inventory = event.getInventory();
         int forgeAnvilValueNew = 0;
         for (int i = 0; i < inventory.getContainerSize(); i++) {
-
-            forgeAnvilValueNew += inventory
+            forgeAnvilValueNew = checkAdd(forgeAnvilValueNew, inventory
                     .getItem(i)
-                    .getOrDefault(forgeAnvilValue, 0);
+                    .getOrDefault(forgeAnvilValue, 0));
         }
         if (forgeAnvilValueNew >= 0) {
-            forgeAnvilValueNew += crafting.getOrDefault(forgeAnvilValue, 0);
-            forgeAnvilValueNew/=crafting.getCount();
+            forgeAnvilValueNew = checkAdd(forgeAnvilValueNew, crafting.getOrDefault(forgeAnvilValue, 0));
+
+            forgeAnvilValueNew/=crafting.getCount();//产物多少平均分配
+
             crafting.set(forgeAnvilValue, forgeAnvilValueNew);
         }
+    }
+
+    public int checkAdd(int k, int v) {
+        if (Integer.MAX_VALUE - k - v < 0) {
+            return ifMax(Integer.MAX_VALUE);
+        } else {
+            return ifMax(k + v);
+        }
+    }
+
+    public int ifMax(int value) {
+        if (value >= Ding.dingConfig.get().base.max_forge_anvil_value) {
+            return Ding.dingConfig.get().base.max_forge_anvil_value;
+        }
+        return value;
     }
 }
