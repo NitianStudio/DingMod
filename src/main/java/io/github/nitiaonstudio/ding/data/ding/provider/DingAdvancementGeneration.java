25de7b4a779ec77acd81a76757c08a930fc0305b
baka4n <qq474899581@gmail.com>
2024-09-18 18:30:13 +0800

    进度触发

--- src/main/java/io/github/nitiaonstudio/ding/data/ding/provider/DingAdvancementGeneration.java
+++ src/main/java/io/github/nitiaonstudio/ding/data/ding/provider/DingAdvancementGeneration.java
@@ -7,8 +7,11 @@ import io.github.nitiaonstudio.ding.registry.TranslateKeyRegistry;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementType;
 import net.minecraft.advancements.CriterionTriggerInstance;
+import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.advancements.critereon.RecipeCraftedTrigger;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.component.DataComponentPredicate;
 import net.minecraft.data.PackOutput;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Items;
@@ -33,21 +36,26 @@ public class DingAdvancementGeneration extends AdvancementProvider {
         super(output, registries, existingFileHelper, List.of((r, s, e) -> {
             Advancement.Builder
                     .advancement()
-                    .addCriterion("craft",
-                            CraftedComponentGetTrigger.TriggerInstance.craftedItem(
-                                    Ding.id("minecraft/merge/", getPath(Items.COBBLESTONE)),
-                                    ComponentRegistry.compression.get()
-                            ))
-                    .display(Items.COBBLESTONE,
-                            TranslateKeyRegistry.mergeCobbleStone,
-                            TranslateKeyRegistry.mergeCobbleStoneDesc,
-                            null,
-                            AdvancementType.GOAL,
-                            true,
-                            true,
-                            false
-                            )
-                    .save(s, Ding.id("minecraft/merge/" + getPath(Items.COBBLESTONE)), e);
+                    .addCriterion("inventory", InventoryChangeTrigger.TriggerInstance.hasItems(
+                            ItemPredicate.Builder.item().hasComponents(DataComponentPredicate.builder().build()).build()
+                    )).save(s, Ding.id("minecraft/amaze"), e);
+//            Advancement.Builder
+//                    .advancement()
+//                    .addCriterion("craft",
+//                            CraftedComponentGetTrigger.TriggerInstance.craftedItem(
+//                                    Ding.id("minecraft/merge/", getPath(Items.COBBLESTONE)),
+//                                    ComponentRegistry.forgeAnvilValue.get()
+//                            ))
+//                    .display(Items.COBBLESTONE,
+//                            TranslateKeyRegistry.mergeCobbleStone,
+//                            TranslateKeyRegistry.mergeCobbleStoneDesc,
+//                            null,
+//                            AdvancementType.GOAL,
+//                            true,
+//                            true,
+//                            false
+//                            )
+//                    .save(s, Ding.id("minecraft/merge/" + getPath(Items.COBBLESTONE)), e);
 
         }));
     }
